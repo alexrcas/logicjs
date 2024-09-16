261c900f6fc22b3e6dce641ec88e18a38c5a1401
Alexis Rodríguez <alexis.rcas@gmail.com>
2024-09-16 18:22:00 +0100

    Aproximación a patrón estrategia

--- script.js
+++ script.js
@@ -1,3 +1,162 @@
+const addLedInterfaceStrategy = element => {
+
+    element.guiA = new Konva.Circle({
+        x: 0,
+        y: 10,
+        radius: 4,
+        fill: 'red',
+    });
+
+    element.group.add(element.guiA);
+    element.figure.on('click', e => console.log(element.gate.id))
+
+    element.guiA.on('click', (e) => {
+        if (!GUIManager.lastClickedInstance) {
+            return;
+        }
+
+        if (!GUIManager.lastClickedInstance.drawing) {
+            return;
+        }
+        console.log('click')
+        GUIManager.lastClickedInstance.drawing = false
+        const origin = GUIManager.lastClickedInstance.gate;
+        const target = element.gate;
+        new Wire(origin, target, 'a', origin.eventEmitter)
+    })
+}
+
+
+
+const addDoubleGateInterfaceStrategy = element => {
+    element.guiA = new Konva.Circle({
+        x: 0,
+        y: 10,
+        radius: 4,
+        fill: 'red',
+    });
+
+    element.guiB = new Konva.Circle({
+        x: 0,
+        y: 30,
+        radius: 4,
+        fill: 'red',
+    });
+
+    element.group.add(element.guiA);
+    element.group.add(element.guiB);
+
+    element.guiS = new Konva.Circle({
+        x: 50,
+        y: 20,
+        radius: 4,
+        fill: 'red',
+    });
+
+    element.group.add(element.guiS);
+
+    let line;
+    let points = [];
+    let lastPoint;
+
+    element.figure.on('click', e => console.log(element.gate.id))
+
+    element.guiS.on('click', (e) => {
+        if (element.drawing == true) {
+            return;
+        }
+
+        GUIManager.lastClickedInstance = element;
+        element.drawing = true;
+        points = [element.guiS.getAbsolutePosition().x, element.guiS.getAbsolutePosition().y]; // Comenzar en el primer círculo
+        lastPoint = { x: element.guiS.getAbsolutePosition().x, y: element.guiS.getAbsolutePosition().y };
+
+        // Crear la línea
+        line = new Konva.Line({
+            points: points,
+            stroke: 'blue',
+            strokeWidth: 2,
+            lineJoin: 'round',
+        });
+        layer.add(line);
+        layer.draw();
+    });
+
+    element.guiA.on('click', (e) => {
+        if (!GUIManager.lastClickedInstance) {
+            return;
+        }
+
+        if (!GUIManager.lastClickedInstance.drawing) {
+            return;
+        }
+        console.log('click')
+        GUIManager.lastClickedInstance.drawing = false
+        const origin = GUIManager.lastClickedInstance.gate;
+        const target = element.gate;
+        new Wire(origin, target, 'a', origin.eventEmitter)
+    })
+
+    element.guiB.on('click', (e) => {
+        if (!GUIManager.lastClickedInstance) {
+            return;
+        }
+
+        if (!GUIManager.lastClickedInstance.drawing) {
+            return;
+        }
+        console.log('click')
+        GUIManager.lastClickedInstance.drawing = false
+        const origin = GUIManager.lastClickedInstance.gate;
+        const target = element.gate;
+        new Wire(origin, target, 'b', origin.eventEmitter)
+
+    })
+
+
+    element.layer.getStage().on('click', (e) => {
+        if (!element.drawing) return;
+
+        const pos = stage.getPointerPosition();
+        const x = pos.x;
+        const y = pos.y;
+
+        if (Math.abs(x - lastPoint.x) > Math.abs(y - lastPoint.y)) {
+            points.push(x, lastPoint.y);
+            lastPoint.x = x;
+        } else {
+            points.push(lastPoint.x, y);
+            lastPoint.y = y;
+        }
+
+        line.points(points);
+        layer.draw();
+    });
+
+
+    element.layer.getStage().on('mousemove', (e) => {
+        if (!element.drawing) return;
+
+        const pos = stage.getPointerPosition();
+        const x = element.snapToGrid(pos.x, element.gridSize);
+        const y = element.snapToGrid(pos.y, element.gridSize);
+
+        let previewPoints = [...points];
+
+        if (Math.abs(x - lastPoint.x) > Math.abs(y - lastPoint.y)) {
+            previewPoints.push(x, lastPoint.y);
+        } else {
+            previewPoints.push(lastPoint.x, y);
+        }
+
+        line.points(previewPoints);
+        layer.draw();
+    });
+
+}
+
+
+
 class EventEmitter {
     constructor() {
         this.events = {};
@@ -54,8 +213,51 @@ class LogicGate {
         this.updateDraw();
         this.eventEmitter.emit(`gate:${this.id}:change`, data);
     }
+}
 
 
+class LogicLED {
+    constructor(eventEmitter) {
+        this._a = 0;
+        this.s = 0;
+        this.eventEmitter = eventEmitter;
+        this.id = Math.random().toString(36).substr(2, 9);
+
+        return new Proxy(this, {
+            set: (target, key, value) => {
+                if (key === 'a') {
+                    target[`_${key}`] = value;
+                    target.updateOutput();
+                } else {
+                    target[key] = value;
+                }
+                return true;
+            },
+            get: (target, key) => {
+                if (key === 'a') {
+                    return target[`_${key}`];
+                } else {
+                    return target[key];
+                }
+            }
+        });
+    }
+
+    updateOutput() {
+        const data = { prev: this.s, s: this.calculateOutput() }
+        this.s = this.calculateOutput();
+        this.updateDraw();
+        this.eventEmitter.emit(`gate:${this.id}:change`, data);
+    }
+}
+
+class LogicLEDImpl extends LogicLED {
+
+    constructor(eventEmitter) { super(eventEmitter) }
+
+    calculateOutput() {
+        return this._a;
+    }
 }
 
 
@@ -97,7 +299,7 @@ class GUIManager {
         this.gate = gate;
         this.layer = layer;
         this.figure = figure;
-        this.group = new Konva.Group({draggable: true})
+        this.group = new Konva.Group({ draggable: true })
         this.group.add(this.figure)
         this.layer.add(this.group);
         this.GRID_SIZE = 10;
@@ -110,145 +312,51 @@ class GUIManager {
             absPos.y = this.snapToGrid(absPos.y);
             this.group.setAbsolutePosition(absPos);
             layer.draw();
-          });
-
-          this.guiA = new Konva.Circle({
-            x: 0,
-            y: 10,
-            radius: 4,
-            fill: 'red',
-          });
-
-
-          this.guiB = new Konva.Circle({
-            x: 0,
-            y: 30,
-            radius: 4,
-            fill: 'red',
-          });
-
-        this.group.add(this.guiA);
-        this.group.add(this.guiB);
-
-
-        this.guiS = new Konva.Circle({
-            x: 50,
-            y: 20,
-            radius: 4,
-            fill: 'red',
-          });
-
-        this.group.add(this.guiS);
-
-
-        let line;
-        let points = [];
-        let lastPoint;
-
-        this.figure.on('click', e => console.log(this.gate.id))
-
-        this.guiS.on('click', (e) => {
-            if (this.drawing == true) {
-                return;
-            }
-
-            GUIManager.lastClickedInstance = this;
-            this.drawing = true;
-            points = [this.guiS.getAbsolutePosition().x, this.guiS.getAbsolutePosition().y]; // Comenzar en el primer círculo
-            lastPoint = { x: this.guiS.getAbsolutePosition().x, y: this.guiS.getAbsolutePosition().y };
-            
-            // Crear la línea
-            line = new Konva.Line({
-              points: points,
-              stroke: 'blue',
-              strokeWidth: 2,
-              lineJoin: 'round',
-            });
-            layer.add(line);
-            layer.draw();
         });
 
-        //para pruebas pero será al hacer click en las entradas
-        this.guiA.on('click', (e) => {
-            if (!GUIManager.lastClickedInstance) {
-                return;
-            }
+        console.log(this.gate instanceof GUILED)
 
-            if (!GUIManager.lastClickedInstance.drawing) {
-                return;
-            }
-            console.log('click')
-            GUIManager.lastClickedInstance.drawing = false
-            const origin = GUIManager.lastClickedInstance.gate;
-            const target = this.gate;
-            new Wire(origin, target, 'a', origin.eventEmitter)
-
-        })
-
-        this.guiB.on('click', (e) => {
-            if (!GUIManager.lastClickedInstance) {
-                return;
-            }
-            
-            if (!GUIManager.lastClickedInstance.drawing) {
-                return;
-            }
-            console.log('click')
-            GUIManager.lastClickedInstance.drawing = false
-            const origin = GUIManager.lastClickedInstance.gate;
-            const target = this.gate;
-            new Wire(origin, target, 'b', origin.eventEmitter)
-
-        })
-
-
-          this.layer.getStage().on('click', (e) => {
-            if (!this.drawing) return;
-          
-            const pos = stage.getPointerPosition();
-            const x = pos.x;
-            const y = pos.y;
-          
-            if (Math.abs(x - lastPoint.x) > Math.abs(y - lastPoint.y)) {
-              points.push(x, lastPoint.y);
-              lastPoint.x = x;
-            } else {
-              points.push(lastPoint.x, y);
-              lastPoint.y = y;
-            }
-          
-            line.points(points);
-            layer.draw();
-        });
-
-
-        this.layer.getStage().on('mousemove', (e) => {
-            if (!this.drawing) return;
-          
-            const pos = stage.getPointerPosition();
-            const x = this.snapToGrid(pos.x, this.gridSize);
-            const y = this.snapToGrid(pos.y, this.gridSize);
-          
-            let previewPoints = [...points];
-          
-            if (Math.abs(x - lastPoint.x) > Math.abs(y - lastPoint.y)) {
-              previewPoints.push(x, lastPoint.y);
-            } else {
-              previewPoints.push(lastPoint.x, y);
-            }
-          
-            line.points(previewPoints);
-            layer.draw();
-          });
+        if (this.gate instanceof GUILED) {
+            addLedInterfaceStrategy(this);
+        } else {
+            console.log('else')
+            addDoubleGateInterfaceStrategy(this);
+        }
 
     }
-
 
     snapToGrid(value) {
         return Math.round(value / this.GRID_SIZE) * this.GRID_SIZE;
     }
 
+}
 
+class GUILED extends LogicLEDImpl {
+
+    constructor(x, y, layer, eventEmitter) {
+        super(eventEmitter);
+        this.x = x;
+        this.y = y;
+        this.layer = layer;
+
+        const figure = new Konva.Path({
+            x: this.x,
+            y: this.y,
+            data: 'M 0 0 L 0 40 L 30 40 A 2 2 90 0 0 30 0 L 0 0',
+            fill: 'cyan',
+        });
+
+        this.GUIManager = new GUIManager(this.layer, figure, this);
+    }
+
+    updateDraw() {
+        if (this.s == 0) {
+            this.GUIManager.figure.fill('cyan');
+        } else {
+            this.GUIManager.figure.fill('yellow');
+        }
+        this.layer.draw();
+    }
 }
 
 
@@ -259,7 +367,7 @@ class GUIGateAND extends LogicGateAND {
         this.x = x;
         this.y = y;
         this.layer = layer;
-        
+
         const figure = new Konva.Path({
             x: this.x,
             y: this.y,
@@ -267,7 +375,7 @@ class GUIGateAND extends LogicGateAND {
             fill: 'blue',
         });
 
-          this.GUIManager = new GUIManager(this.layer, figure, this);
+        this.GUIManager = new GUIManager(this.layer, figure, this);
 
     }
 
@@ -291,7 +399,7 @@ class GUIGateOR extends LogicGateOR {
         this.x = x;
         this.y = y;
         this.layer = layer;
-        
+
         const figure = new Konva.Path({
             x: this.x,
             y: this.y,
@@ -299,7 +407,7 @@ class GUIGateOR extends LogicGateOR {
             fill: 'blue',
         });
 
-          this.GUIManager = new GUIManager(this.layer, figure, this);
+        this.GUIManager = new GUIManager(this.layer, figure, this);
     }
 
 
@@ -343,6 +451,8 @@ class GateFactory {
 
     gateOR() { return new GUIGateOR(0, 0, layer, this.eventEmitter) }
 
+    led() { return new GUILED(0, 0, layer, this.eventEmitter) }
+
     wire(outputGate, inputGate, inputName) { return new Wire(outputGate, inputGate, inputName, this.eventEmitter) }
 
 }
@@ -366,6 +476,8 @@ const gate2 = factory.gateOR();
 const gate3 = factory.gateAND();
 const gate4 = factory.gateOR();
 
+const led = factory.led();
+
 
 gates.push(gate1);
 gates.push(gate2);
@@ -382,3 +494,5 @@ document.querySelector('#send').addEventListener('click', () => {
     const inputname = document.querySelector('#inputname').value;
     const gate = gates.filter(el => el.id == id)[0][inputname] = value
 })
+
+
