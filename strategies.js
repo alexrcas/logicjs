a44907c645e90aa29574f0f14d73aaf5dddf5e29
Alexis Rodríguez <alexis.rcas@gmail.com>
2024-09-17 18:36:23 +0100

    Refactor

--- strategies.js
+++ strategies.js
@@ -1,47 +1,165 @@
+
+class Input {
+
+    constructor(name, gate, x, y) {
+
+        this.name = name;
+        this.gate = gate;
+
+        this.figure = new Konva.Circle({
+            x: x,
+            y: y,
+            radius: 4,
+            fill: 'grey',
+        });
+
+        this.figure.on('mouseover', () => { 
+            document.body.style.cursor = 'pointer';
+            this.figure.radius(8);
+            this.figure.fill('blue');
+        });
+
+        this.figure.on('mouseout', () => {
+            document.body.style.cursor = 'default';
+            this.figure.radius(4);
+            this.figure.fill('grey');
+        });
+
+        this.figure.on('click', (e) => {
+            if (!GUIManager.lastClickedInstance) {
+                return;
+            }
+    
+            if (!GUIManager.lastClickedInstance.drawing) {
+                return;
+            }
+            GUIManager.lastClickedInstance.drawing = false
+            const origin = GUIManager.lastClickedInstance.gate;
+            const target = this.gate;
+            new Wire(origin, target, this.name, origin.eventEmitter)
+        })
+
+    }
+}
+
+
+class Output {
+    constructor(gate, element) {
+
+        this.gate = gate;
+        this.element = element;
+
+        this.figure = new Konva.Circle({
+            x: 50,
+            y: 20,
+            radius: 4,
+            fill: 'grey',
+        });
+
+        this.figure.on('mouseover', () => { 
+            document.body.style.cursor = 'pointer';
+            this.figure.radius(8);
+            this.figure.fill('blue');
+        });
+    
+        this.figure.on('mouseout', () => {
+            document.body.style.cursor = 'default';
+            this.figure.radius(4);
+            this.figure.fill('grey');
+        });
+
+        let line;
+        let points = [];
+        let lastPoint;
+
+        this.figure.on('click', (e) => {
+
+            if (this.element.drawing == true) {
+                return;
+            }
+    
+            GUIManager.lastClickedInstance = this.element;
+            this.element.drawing = true;
+            points = [this.figure.getAbsolutePosition().x, this.figure.getAbsolutePosition().y]; // Comenzar en el primer círculo
+            lastPoint = { x: this.figure.getAbsolutePosition().x, y: this.figure.getAbsolutePosition().y };
+    
+            // Crear la línea
+            line = new Konva.Line({
+                points: points,
+                stroke: '#264653',
+                strokeWidth: 1,
+                lineJoin: 'round',
+            });
+            layer.add(line);
+            layer.draw();
+        });
+
+
+        window.addEventListener('keydown', (e) => {
+            if (e.key === 'Escape' && element.drawing) {
+                // Cancelar el dibujo de la línea
+                element.drawing = false;
+                if (line) {
+                    line.destroy();  // Eliminar la línea de la capa
+                    layer.draw();    // Redibujar la capa
+                }
+            }
+        });
+    
+    
+        this.element.layer.getStage().on('click', (e) => {
+            if (!element.drawing) return;
+    
+            const pos = stage.getPointerPosition();
+            const x = pos.x;
+            const y = pos.y;
+    
+            if (Math.abs(x - lastPoint.x) > Math.abs(y - lastPoint.y)) {
+                points.push(x, lastPoint.y);
+                lastPoint.x = x;
+            } else {
+                points.push(lastPoint.x, y);
+                lastPoint.y = y;
+            }
+    
+            line.points(points);
+            this.element.layer.draw();
+        });
+    
+    
+        this.element.layer.getStage().on('mousemove', (e) => {
+            if (!element.drawing) return;
+    
+            const pos = stage.getPointerPosition();
+            const x = element.snapToGrid(pos.x, element.gridSize);
+            const y = element.snapToGrid(pos.y, element.gridSize);
+    
+            let previewPoints = [...points];
+    
+            if (Math.abs(x - lastPoint.x) > Math.abs(y - lastPoint.y)) {
+                previewPoints.push(x, lastPoint.y);
+            } else {
+                previewPoints.push(lastPoint.x, y);
+            }
+    
+            line.points(previewPoints);
+            this.element.layer.draw();
+        });
+    }
+}
+
+
+
 const addLedInterfaceStrategy = element => {
-
-    element.guiA = new Konva.Circle({
-        x: 18,
-        y: 44,
-        radius: 4,
-        fill: 'grey',
-    });
-
-    element.group.add(element.guiA);
+    const inputA = new Input('a', element.gate, 18, 44);
+    element.group.add(inputA.figure);
     element.figure.on('click', e => console.log(element.gate.id))
-
-    element.guiA.on('mouseover', () => { 
-        document.body.style.cursor = 'pointer';
-        element.guiA.radius(8);
-        element.guiA.fill('blue');
-    });
-
-    element.guiA.on('mouseout', function() {
-        document.body.style.cursor = 'default';
-        element.guiA.radius(4);
-        element.guiA.fill('grey');
-    });
-
-    element.guiA.on('click', (e) => {
-        if (!GUIManager.lastClickedInstance) {
-            return;
-        }
-
-        if (!GUIManager.lastClickedInstance.drawing) {
-            return;
-        }
-        GUIManager.lastClickedInstance.drawing = false
-        const origin = GUIManager.lastClickedInstance.gate;
-        const target = element.gate;
-        new Wire(origin, target, 'a', origin.eventEmitter)
-    })
 }
 
 
 const addSwitchInterfaceStrategy = element => {
 
     element.figure.on('click', e => {
-        console.log(element.gate.id)
         element.gate.a = !element.gate.a;
     })
 
@@ -53,271 +171,20 @@ const addSwitchInterfaceStrategy = element => {
         document.body.style.cursor = 'default';
     })
 
-    element.guiS = new Konva.Circle({
-        x: 50,
-        y: 20,
-        radius: 4,
-        fill: 'grey',
-    });
-
-    element.group.add(element.guiS);
-
-    element.guiS.on('mouseover', () => { 
-        document.body.style.cursor = 'pointer';
-        element.guiS.radius(8);
-        element.guiS.fill('blue');
-    });
-
-    element.guiS.on('mouseout', function() {
-        document.body.style.cursor = 'default';
-        element.guiS.radius(4);
-        element.guiS.fill('grey');
-    });
-
-    element.guiS.on('click', (e) => {
-        if (element.drawing == true) {
-            return;
-        }
-
-        GUIManager.lastClickedInstance = element;
-        element.drawing = true;
-        points = [element.guiS.getAbsolutePosition().x, element.guiS.getAbsolutePosition().y]; // Comenzar en el primer círculo
-        lastPoint = { x: element.guiS.getAbsolutePosition().x, y: element.guiS.getAbsolutePosition().y };
-
-        // Crear la línea
-        line = new Konva.Line({
-            points: points,
-            stroke: '#264653',
-            strokeWidth: 1,
-            lineJoin: 'round',
-        });
-        layer.add(line);
-        layer.draw();
-    });
-
-    window.addEventListener('keydown', (e) => {
-        if (e.key === 'Escape' && element.drawing) {
-            // Cancelar el dibujo de la línea
-            element.drawing = false;
-            if (line) {
-                line.destroy();  // Eliminar la línea de la capa
-                layer.draw();    // Redibujar la capa
-            }
-        }
-    });
-
-
-    element.layer.getStage().on('click', (e) => {
-        if (!element.drawing) return;
-
-        const pos = stage.getPointerPosition();
-        const x = pos.x;
-        const y = pos.y;
-
-        if (Math.abs(x - lastPoint.x) > Math.abs(y - lastPoint.y)) {
-            points.push(x, lastPoint.y);
-            lastPoint.x = x;
-        } else {
-            points.push(lastPoint.x, y);
-            lastPoint.y = y;
-        }
-
-        line.points(points);
-        layer.draw();
-    });
-
-
-    element.layer.getStage().on('mousemove', (e) => {
-        if (!element.drawing) return;
-
-        const pos = stage.getPointerPosition();
-        const x = element.snapToGrid(pos.x, element.gridSize);
-        const y = element.snapToGrid(pos.y, element.gridSize);
-
-        let previewPoints = [...points];
-
-        if (Math.abs(x - lastPoint.x) > Math.abs(y - lastPoint.y)) {
-            previewPoints.push(x, lastPoint.y);
-        } else {
-            previewPoints.push(lastPoint.x, y);
-        }
-
-        line.points(previewPoints);
-        layer.draw();
-    });
+    const output = new Output(element.gate, element);
+    element.group.add(output.figure);
 }
 
 
 
 const addDoubleGateInterfaceStrategy = element => {
-    element.guiA = new Konva.Circle({
-        x: 0,
-        y: 10,
-        radius: 4,
-        fill: 'grey',
-    });
 
-    element.guiA.on('mouseover', () => { 
-        document.body.style.cursor = 'pointer';
-        element.guiA.radius(8);
-        element.guiA.fill('blue');
-    });
+    const inputA = new Input('a', element.gate, 0, 10);
+    const inputB = new Input('b', element.gate, 0, 30);
 
-    element.guiA.on('mouseout', function() {
-        document.body.style.cursor = 'default';
-        element.guiA.radius(4);
-        element.guiA.fill('grey');
-    });
+    element.group.add(inputA.figure);
+    element.group.add(inputB.figure);
 
-    element.guiB = new Konva.Circle({
-        x: 0,
-        y: 30,
-        radius: 4,
-        fill: 'grey',
-    });
-
-
-    element.guiB.on('mouseover', () => { 
-        document.body.style.cursor = 'pointer';
-        element.guiB.radius(8);
-        element.guiB.fill('blue');
-    });
-
-    element.guiB.on('mouseout', function() {
-        document.body.style.cursor = 'default';
-        element.guiB.radius(4);
-        element.guiB.fill('grey');
-    });
-
-    element.group.add(element.guiA);
-    element.group.add(element.guiB);
-
-    element.guiS = new Konva.Circle({
-        x: 50,
-        y: 20,
-        radius: 4,
-        fill: 'grey',
-    });
-
-    element.guiS.on('mouseover', () => { 
-        document.body.style.cursor = 'pointer';
-        element.guiS.radius(8);
-        element.guiS.fill('blue');
-    });
-
-    element.guiS.on('mouseout', function() {
-        document.body.style.cursor = 'default';
-        element.guiS.radius(4);
-        element.guiS.fill('grey');
-    });
-
-    element.group.add(element.guiS);
-
-    let line;
-    let points = [];
-    let lastPoint;
-
-    element.figure.on('click', e => console.log(element.gate.id))
-
-    element.guiS.on('click', (e) => {
-        if (element.drawing == true) {
-            return;
-        }
-
-        GUIManager.lastClickedInstance = element;
-        element.drawing = true;
-        points = [element.guiS.getAbsolutePosition().x, element.guiS.getAbsolutePosition().y]; // Comenzar en el primer círculo
-        lastPoint = { x: element.guiS.getAbsolutePosition().x, y: element.guiS.getAbsolutePosition().y };
-
-        // Crear la línea
-        line = new Konva.Line({
-            points: points,
-            stroke: '#264653',
-            strokeWidth: 1,
-            lineJoin: 'round',
-        });
-        layer.add(line);
-        layer.draw();
-    });
-
-    element.guiA.on('click', (e) => {
-        if (!GUIManager.lastClickedInstance) {
-            return;
-        }
-
-        if (!GUIManager.lastClickedInstance.drawing) {
-            return;
-        }
-        GUIManager.lastClickedInstance.drawing = false
-        const origin = GUIManager.lastClickedInstance.gate;
-        const target = element.gate;
-        new Wire(origin, target, 'a', origin.eventEmitter)
-    })
-
-    element.guiB.on('click', (e) => {
-        if (!GUIManager.lastClickedInstance) {
-            return;
-        }
-
-        if (!GUIManager.lastClickedInstance.drawing) {
-            return;
-        }
-        GUIManager.lastClickedInstance.drawing = false
-        const origin = GUIManager.lastClickedInstance.gate;
-        const target = element.gate;
-        new Wire(origin, target, 'b', origin.eventEmitter)
-
-    })
-
-
-    element.layer.getStage().on('click', (e) => {
-        if (!element.drawing) return;
-
-        const pos = stage.getPointerPosition();
-        const x = pos.x;
-        const y = pos.y;
-
-        if (Math.abs(x - lastPoint.x) > Math.abs(y - lastPoint.y)) {
-            points.push(x, lastPoint.y);
-            lastPoint.x = x;
-        } else {
-            points.push(lastPoint.x, y);
-            lastPoint.y = y;
-        }
-
-        line.points(points);
-        layer.draw();
-    });
-
-
-    element.layer.getStage().on('mousemove', (e) => {
-        if (!element.drawing) return;
-
-        const pos = stage.getPointerPosition();
-        const x = element.snapToGrid(pos.x, element.gridSize);
-        const y = element.snapToGrid(pos.y, element.gridSize);
-
-        let previewPoints = [...points];
-
-        if (Math.abs(x - lastPoint.x) > Math.abs(y - lastPoint.y)) {
-            previewPoints.push(x, lastPoint.y);
-        } else {
-            previewPoints.push(lastPoint.x, y);
-        }
-
-        line.points(previewPoints);
-        layer.draw();
-    });
-
-    window.addEventListener('keydown', (e) => {
-        if (e.key === 'Escape' && element.drawing) {
-            // Cancelar el dibujo de la línea
-            element.drawing = false;
-            if (line) {
-                line.destroy();  // Eliminar la línea de la capa
-                layer.draw();    // Redibujar la capa
-            }
-        }
-    });
-
+    const output = new Output(element.gate, element);
+    element.group.add(output.figure);
 }
