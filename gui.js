0c078c5e602cdfed31680b166b704544a755662c
Alexis Rodríguez <alexis.rcas@gmail.com>
2024-09-17 12:45:54 +0100

    Corregir colores

--- gui.js
+++ gui.js
@@ -2,6 +2,9 @@ class GUIManager {
 
     static lastClickedInstance = null;
 
+    static offColor = '#219ebc';
+    static onColor = '#e9c46a';
+
     constructor(layer, figure, gate) {
         this.gate = gate;
         this.layer = layer;
@@ -52,7 +55,7 @@ class GUILED extends LogicLEDImpl {
             x: this.x,
             y: this.y,
             data: 'M 15.3022 14.8133 h 5.1924 l -2.5962 4.4968 L 15.3022 14.8133 z M 34.8048 17.2661 c 0 6.1041 -3.1257 11.6334 -8.3616 14.7905 c -0.9646 0.5816 -1.5639 1.6369 -1.5639 2.7538 v 3.2643 c 0 1.8143 -1.0728 3.3786 -2.6152 4.104 c 0.0207 0.1023 0.0319 0.208 0.0319 0.3164 c 0 0.8739 -0.7081 1.582 -1.582 1.582 h -6.3513 c -0.8737 0 -1.582 -0.7081 -1.582 -1.582 c 0 -0.1084 0.011 -0.2141 0.0319 -0.3164 c -1.5424 -0.7252 -2.6152 -2.2897 -2.6152 -4.104 V 34.811 c 0 -1.1323 -0.5925 -2.1665 -1.5848 -2.7669 c -5.2954 -3.2035 -8.4892 -9.0342 -8.3352 -15.2172 c 0.1101 -4.4276 1.9121 -8.6052 5.0741 -11.7631 C 8.5134 1.9062 12.6931 0.1093 17.1207 0.0046 C 26.8261 -0.219 34.8048 7.607 34.8048 17.2661 z M 24.6047 12.4402 c -0.2827 -0.4894 -0.8047 -0.791 -1.37 -0.791 H 12.5621 c -0.5653 0 -1.0874 0.3016 -1.37 0.791 s -0.2827 1.0927 0 1.582 l 5.1243 8.8758 v 9.912 c 0 0.8739 0.7083 1.582 1.582 1.582 c 0.8739 0 1.582 -0.7081 1.582 -1.582 v -9.912 l 5.1243 -8.8758 C 24.8874 13.5329 24.8874 12.9298 24.6047 12.4402 z',
-            fill: 'blue',
+            fill: GUIManager.offColor
         });
 
         this.GUIManager = new GUIManager(this.layer, figure, this);
@@ -60,9 +63,9 @@ class GUILED extends LogicLEDImpl {
 
     updateDraw() {
         if (this.s == 0) {
-            this.GUIManager.figure.fill('blue');
+            this.GUIManager.figure.fill(GUIManager.offColor);
         } else {
-            this.GUIManager.figure.fill('green');
+            this.GUIManager.figure.fill(GUIManager.onColor);
         }
         this.layer.draw();
     }
@@ -81,7 +84,7 @@ class GUISwitch extends LogicSwitchImpl {
             x: this.x,
             y: this.y,
             data: 'M 42 0 H 2 c -1.1 0 -2 0.9 -2 2 v 32 c 0 1.1 0.9 2 2 2 h 40 c 1.1 0 2 -0.9 2 -2 V 2 C 44 0.9 43.1 0 42 0 z M 40 32 H 4 V 4 h 36 V 32 z M 17 30 h -9.9 c -0.6 0 -1 -0.4 -1 -1 V 7 c 0 -0.6 0.4 -1 1 -1 H 17 c 0.6 0 1 0.4 1 1 v 22 C 18 29.6 17.6 30 17 30 z',
-            fill: 'blue',
+            fill: GUIManager.offColor,
         });
 
         this.GUIManager = new GUIManager(this.layer, figure, this);
@@ -89,10 +92,10 @@ class GUISwitch extends LogicSwitchImpl {
 
     updateDraw() {
         if (this.s == 0) {
-            this.GUIManager.figure.fill('blue');
+            this.GUIManager.figure.fill(GUIManager.offColor);
             this.GUIManager.figure.data('M 42 0 H 2 c -1.1 0 -2 0.9 -2 2 v 32 c 0 1.1 0.9 2 2 2 h 40 c 1.1 0 2 -0.9 2 -2 V 2 C 44 0.9 43.1 0 42 0 z M 40 32 H 4 V 4 h 36 V 32 z M 17 30 h -9.9 c -0.6 0 -1 -0.4 -1 -1 V 7 c 0 -0.6 0.4 -1 1 -1 H 17 c 0.6 0 1 0.4 1 1 v 22 C 18 29.6 17.6 30 17 30 z')
         } else {
-            this.GUIManager.figure.fill('green');
+            this.GUIManager.figure.fill(GUIManager.onColor);
             this.GUIManager.figure.data('M 42 0 H 2 c -1.1 0 -2 0.9 -2 2 v 32 c 0 1.1 0.9 2 2 2 h 40 c 1.1 0 2 -0.9 2 -2 V 2 C 44 0.9 43.1 0 42 0 z M 40 32 H 4 V 4 h 36 V 32 z M 37 30 h -9.9 c -0.6 0 -1 -0.4 -1 -1 V 7 c 0 -0.6 0.4 -1 1 -1 H 37 c 0.6 0 1 0.4 1 1 v 22 C 38 29.6 37.6 30 37 30 z')
         }
         this.layer.draw();
@@ -114,7 +117,7 @@ class GUIGateAND extends LogicGateAND {
             x: this.x,
             y: this.y,
             data: 'M 0 0 L 0 40 L 30 40 A 2 2 90 0 0 30 0 L 0 0',
-            fill: 'blue',
+            fill: GUIManager.offColor,
         });
 
         this.GUIManager = new GUIManager(this.layer, figure, this);
@@ -123,9 +126,9 @@ class GUIGateAND extends LogicGateAND {
 
     updateDraw() {
         if (this.s == 0) {
-            this.GUIManager.figure.fill('blue');
+            this.GUIManager.figure.fill(GUIManager.offColor);
         } else {
-            this.GUIManager.figure.fill('green');
+            this.GUIManager.figure.fill(GUIManager.onColor);
         }
         this.layer.draw();
     }
@@ -145,7 +148,7 @@ class GUIGateOR extends LogicGateOR {
             x: this.x,
             y: this.y,
             data: 'M 0 0 Q 25 16 0 40 L 30 40 A 2 2 90 0 0 30 0 L 0 0',
-            fill: 'blue',
+            fill: GUIManager.offColor,
         });
 
         this.GUIManager = new GUIManager(this.layer, figure, this);
@@ -154,9 +157,9 @@ class GUIGateOR extends LogicGateOR {
 
     updateDraw() {
         if (this.s == 0) {
-            this.GUIManager.figure.fill('blue');
+            this.GUIManager.figure.fill(GUIManager.offColor);
         } else {
-            this.GUIManager.figure.fill('green');
+            this.GUIManager.figure.fill(GUIManager.onColor);
         }
         this.layer.draw();
     }
@@ -175,7 +178,7 @@ class GUIGateXOR extends LogicGateXOR {
             x: this.x,
             y: this.y,
             data: 'M 0 0 Q 25 16 0 40 L 30 40 A 2 2 90 0 0 30 0 L 0 0',
-            fill: 'brown',
+            fill: GUIManager.offColor,
         });
 
         this.GUIManager = new GUIManager(this.layer, figure, this);
@@ -184,9 +187,9 @@ class GUIGateXOR extends LogicGateXOR {
 
     updateDraw() {
         if (this.s == 0) {
-            this.GUIManager.figure.fill('blue');
+            this.GUIManager.figure.fill(GUIManager.offColor);
         } else {
-            this.GUIManager.figure.fill('green');
+            this.GUIManager.figure.fill(GUIManager.onColor);
         }
         this.layer.draw();
     }
